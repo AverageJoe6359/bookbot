a22cc2a13bd275605ee07bda1a53acde0d071942
AverageJoe6359 <homer.joseph69@gmail.com>
2025-06-07 15:23:44 -0400

    get_book_text = working

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     print(get_book_text("books/frankenstein.txt"))
     
 def get_book_text(filepath):
-    with open("frankenstein.txt", encoding="utf-8") as f:
+    with open("books/frankenstein.txt") as f:
         book_text = f.read()
+        return book_text
 main()
